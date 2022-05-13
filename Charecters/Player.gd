f655a951ee60613aa3239980bee880e9bd4ff857
Yoosuf Sayyid <cena26657@gmail.com>
2022-05-13 14:54:44 +0500

    k

--- Charecters/Player.gd
+++ Charecters/Player.gd
@@ -2,11 +2,14 @@ extends KinematicBody2D
 
 export(float) var move_speed = 200
 export(float) var jump_impulse = 600
+export(int) var max_jumps = 2
 
-enum STATE {IDLE, RUN, JUMP}
+enum STATE {IDLE, RUN, JUMP, DOUBLE_JUMP}
 
 onready var animated_sprite = $AnimatedSprite
 onready var animation_tree = $AnimationTree
+
+signal changed_state(new_state_string, new_state_id)
 
 var velocity: Vector2
 
@@ -34,7 +37,8 @@ func adjust_flip_direction(input : Vector2):
 	
 	
 func set_anim_parameters():
-	animation_tree.set("parameters/x_move/blend_position", sign(velocity.x))
+	animation_tree.set("parameters/x_sign/blend_position", sign(velocity.x))
+	animation_tree.set("parameters/y_sign/blend_amount", sign(velocity.y))
 
 	
 func pick_next_state():
@@ -51,7 +55,8 @@ func pick_next_state():
 	
 	else:
 		# to do double jump
-		pass
+		if (Input.is_action_just_pressed("jump") && jumps < max_jumps):
+			self.current_state = STATE.DOUBLE_JUMP
 		
 # uses input to determine which directions the player is pressing down on for use in movement
 func get_player_input():
@@ -69,8 +74,9 @@ func jump():
 # setters
 func set_current_state(new_state):
 	match(new_state):
-		STATE.JUMP:
+		STATE.JUMP, STATE.DOUBLE_JUMP:
 			jump()
 	
 	current_state = new_state
-		
+	emit_signal("changed_state",STATE.keys()[new_state] ,new_state)
+
