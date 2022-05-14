ca311a62a77b8ee551146cb4639f77d6759eb24c
Yoosuf Sayyid <cena26657@gmail.com>
2022-05-14 11:30:39 +0500

    k

--- Charecters/Enemies/AngryPig.gd
+++ Charecters/Enemies/AngryPig.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-export(float) var move_speed = 100
+enum STATE {WALK, RUN}
+
+export(float) var walk_speed = 100
 export(float) var run_speed = 200
 export(float) var waypoint_arrived_distance = 10
 export(bool) var faces_right = true
@@ -11,8 +13,10 @@ export(int) var starting_waypoint = 0
 var waypoint_position
 var waypoint_index setget set_waypoint_index
 var velocity = Vector2.ZERO
+var current_state = STATE.WALK
 
 onready var animated_sprite = $AnimatedSprite
+onready var animation_tree = $AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,6 +30,14 @@ func _physics_process(delta):
 	if(distance_x >= waypoint_arrived_distance):
 		#move towards waypoint
 		var direction_x_sign = sign(direction.x)
+		
+		var move_speed: float
+		
+		match(current_state):
+			STATE.WALK:
+				move_speed = walk_speed
+			STATE.RUN:
+				move_speed = run_speed
 		
 		velocity = Vector2(
 			move_speed * sign (direction.x),
@@ -51,3 +63,13 @@ func _physics_process(delta):
 func set_waypoint_index(value):
 	waypoint_index = value
 	waypoint_position = get_node(waypoints[waypoint_index]).position
+
+
+func _on_AngryDetectionZone_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	animation_tree.set("parameters/player_detected/blend_position", 1)
+	current_state = STATE.RUN
+
+
+func _on_AngryDetectionZone_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	animation_tree.set("parameters/player_detected/blend_position", 0)
+	current_state = STATE.WALK
