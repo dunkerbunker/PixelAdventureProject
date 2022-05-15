9c1b5ff2aa732e229009a53305f5903e9e985b92
Yoosuf Sayyid <cena26657@gmail.com>
2022-05-15 17:26:03 +0500

    k

--- Charecters/Player.gd
+++ Charecters/Player.gd
@@ -4,15 +4,18 @@ export(float) var move_speed = 200
 export(float) var jump_impulse = 600
 export(float) var enemy_bounce_impulse = 600
 export(int) var max_jumps = 2
+export(float) var wall_slide_friction = 0.1
 export(float) var jump_damage = 1
 export(float) var knockback_collision_speed = 100
 
-enum STATE {IDLE, RUN, JUMP, DOUBLE_JUMP, HIT}
+
+enum STATE {IDLE, RUN, JUMP, DOUBLE_JUMP, HIT, WALL_SLIDE}
 
 onready var animated_sprite = $AnimatedSprite
 onready var animation_tree = $AnimationTree
 onready var jump_hitbox = $JumpHitbox
 onready var invincible_timer = $InvincibleTimer
+onready var wall_jump_timer = $WallJumpTimer
 
 signal changed_state(new_state_string, new_state_id)
 
@@ -21,21 +24,31 @@ var velocity: Vector2
 var current_state = STATE.IDLE setget set_current_state
 var jumps = 0
 var has_double_jump_occured = false
+var is_bordering_wall : bool
+var wall_jump_direction : Vector2
 
 func _physics_process(delta):
 	var input = get_player_input()
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	is_bordering_wall = get_is_on_wall_raycast_test()
 	set_anim_parameters()
 	
 	match (current_state):
 		STATE.IDLE, STATE.JUMP, STATE.RUN, STATE.DOUBLE_JUMP:
-			velocity = normal_move(input)
+			if (wall_jump_timer.is_stopped()):	
+				velocity = normal_move(input)
+			else:
+				velocity = wall_jumping_movement()
 			pick_next_state()
 		STATE.HIT:
 			velocity = hit_move()
+		STATE.WALL_SLIDE:
+			velocity = wall_slide_move()
+			pick_next_state()
 	
-	
+
 func normal_move(input):
 	adjust_flip_direction(input)
 	return Vector2(
@@ -57,6 +70,18 @@ func hit_move():
 	
 	return knockback_collision_speed * knockback_direction
 
+func wall_slide_move():
+	return Vector2(
+		velocity.x,
+		min(velocity.y + (GameSettings.gravity * wall_slide_friction), GameSettings.terminal_velocity)
+	)
+
+func wall_jumping_movement():
+	return Vector2(
+		move_speed * wall_jump_direction.x,
+		min(velocity.y + GameSettings.gravity, GameSettings.terminal_velocity)
+	)
+
 func adjust_flip_direction(input : Vector2):
 	if (sign(input.x) == 1):
 		animated_sprite.flip_h = false
@@ -68,6 +93,14 @@ func set_anim_parameters():
 	animation_tree.set("parameters/x_sign/blend_position", sign(velocity.x))
 	animation_tree.set("parameters/y_sign/blend_amount", sign(velocity.y))
 
+	var is_on_wall_int: int
+	
+	if (is_bordering_wall):
+		is_on_wall_int = 1
+	else:
+		is_on_wall_int = 0
+		
+	animation_tree.set("parameters/is_on_wall/blend_amount", is_on_wall_int)
 	
 func pick_next_state():
 	if (is_on_floor()):
@@ -82,10 +115,20 @@ func pick_next_state():
 		else:
 			self.current_state = STATE.IDLE
 	
-	else:
-		# to do double jump
-		if (Input.is_action_just_pressed("jump") && jumps < max_jumps && has_double_jump_occured == false):
+	# to do double jump
+	elif (Input.is_action_just_pressed("jump") && jumps < max_jumps && has_double_jump_occured == false):
+		if (is_bordering_wall):
+			self.current_state = STATE.JUMP
+		else:
 			self.current_state = STATE.DOUBLE_JUMP
+	
+	# is bordering wall
+	elif (is_bordering_wall):
+		self.current_state = STATE.WALL_SLIDE
+		
+	elif (self.current_state == STATE.WALL_SLIDE && !is_bordering_wall):
+		self.current_state = STATE.JUMP
+		
 		
 # uses input to determine which directions the player is pressing down on for use in movement
 func get_player_input():
@@ -95,10 +138,35 @@ func get_player_input():
 	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	return input
+
+# returns direction the charecter is facing  based on the flip direction of animated sprite
+func get_facing_direction():
+	if (animated_sprite.flip_h == false):
+		return Vector2.RIGHT
+	else:
+		return Vector2.LEFT
+	
+# checks for walls in the direction the charecter is facing using raycast
+# returns true if it fines a wall, null otherwise
+# only collision mask is checked
+func get_is_on_wall_raycast_test():
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(global_position, global_position + 10 * get_facing_direction(), [self], self.collision_mask)	
+	
+	if (result.size() > 0):
+		return true
+	else:
+		return false
 	
 func jump():
 	velocity.y = -jump_impulse
 	jumps += 1
+	
+func wall_jump():
+	velocity.y = -jump_impulse
+	jumps = 1
+	wall_jump_direction = -get_facing_direction()
+	wall_jump_timer.start()
 	
 func _on_JumpHitbox_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	var enemy = area.owner
@@ -123,13 +191,23 @@ func on_hit_finished():
 func set_current_state(new_state):
 	match(new_state):
 		STATE.JUMP:
-			jump()
+			# multiple ways to enter jump state from wall slide state check pick_next_state
+			if (current_state == STATE.WALL_SLIDE):
+				if (Input.is_action_just_pressed("jump")):
+					wall_jump()
+			else:
+				jump()
 		STATE.DOUBLE_JUMP:
 			jump()
 			animation_tree.set("parameters/double_jump/active", true)
 			has_double_jump_occured = true
 		STATE.HIT:
 			animation_tree.set("parameters/hit/active", true)
+		STATE.WALL_SLIDE:
+			jumps = 0
+			# animation_tree.set("parameters/is_on_wall/blend_amount", 1)
+			# above code is set in set_anim_parameters function as it needs to be refreshed every frame. 
+			# Will be called in physics function to update animation tree every frame
 		
 	current_state = new_state
 	emit_signal("changed_state",STATE.keys()[new_state] ,new_state)
